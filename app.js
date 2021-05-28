098ac01308d0c3fcb080fca6064fc0e96e0f0763
sig Lec <slecaudey@gmail.com>
2021-05-28 22:51:30 +0200

    fonctionnalités boutons

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
 $(document).ready(function(){
 
 //var $title = $('#title'),
-//var $author = $('#author'),
-//var $submit = $('#submit');
-//var $reset = $('#reset');
-//var $addBook = $('#addBook');
 
-$('.h2').after('<button class="btn" id="addBook">Ajouter un livre</button>');
+var addButton = $('.h2').after('<button class="btn" id="addBook">Ajouter un livre</button>');
 	$('#addBook').css({
 		color : 'rgb(255,255,255)',
 		backgroundColor : 'rgb(23, 152, 155)',
@@ -16,58 +12,52 @@ $('.h2').after('<button class="btn" id="addBook">Ajouter un livre</button>');
 	});
 
 
-$('<div id="addBookForm"></div>').insertAfter('.btn');
+var addBookForm = $('.h2').after('<div id="addBookForm"></div>')
 	$('#addBookForm').css({
-		backgroundColor : 'rgb(224, 224, 224)',
+		backgroundColor : 'rgb(205, 224, 224)',
 });
 
-function displaySearchForm(){
-$('#addBookForm').prepend(
-	'<div id="formDisplay">',
+$('#addBookForm').css('display', 'none');
+
+$('#addBook').click (function(){
+	$('#addBook').hide();
+	$('#addBookForm').css('display', 'block');
+
+
+});
+
+$('#addBookForm').append(
 	'<form id="formSearch">',
 	'<label for="title" class="label">Titre du livre</label><br /><br />', 
 	'<input type="text" id="title" class="field" required /><br /><br />',
 	'<label for="author" class="label">Auteur</label><br /><br />', 
 	'<input type="text" id="author" class="field" required /><br /><br />',
-	'</form>',
-	'<script>',
-	'console.log("test")',
-	'</script>',
-
 	'<button class="btnForm" id="search">Rechercher</button><br /><br />',
 	'<button class="btnForm" id="cancel">Annuler</button><br /><br />',
-	'</div>');
-	//'<input type="submit" id="submit" class="btnForm" value="Rechercher" /><br /><br />',
-	//'<input type="reset" id="reset" class="btnForm" value="Annuler" /><br /><br />',
-}
+	'</form>');
 
-$('#addBook').click(function(){
-	$('#addBook').hide();
-	displaySearchForm();
-});
+/*	$('#addBook').hide();
+	displaySearchForm();*/
 
-
-
-$('#cancel').click(function(){
+$('#cancel').click (function(){
+	$('#addBookForm').css('display', 'none');
 	$('#addBook').show();
 });
 
 
 
-});
-
-/* 1. Créer une arboresence des méthodes utiles
-   2. Variabiliser un maximum
-*/
-
-
 /*function displaySearchForm(){
-$('#addBookForm').prepend('<div id="formDisplay"></div>');
-$('#formDisplay').append('<form id="formSearch"></form>');
-$('#formSearch').append('<label for="title" class="label">Titre du livre</label><br /><br />');
-$('#formSearch').append('<input type="text" id="title" class="field" required /><br /><br />');
-$('#formSearch').append('<label for="author" class="label">Auteur</label><br /><br />');
-$('#formSearch').append('<input type="text" id="author" class="field" required /><br /><br />');
-$('#formSearch').append('<input type="submit" id="submit" class="btnForm" value="Rechercher" /><br /><br />');
-$('#formSearch').append('<input type="reset" id="reset" class="btnForm" value="Annuler" /><br /><br />');
+$('#addBookForm').prepend(
+	'<form id="formSearch">',
+	'<label for="title" class="label">Titre du livre</label><br /><br />', 
+	'<input type="text" id="title" class="field" required /><br /><br />',
+	'<label for="author" class="label">Auteur</label><br /><br />', 
+	'<input type="text" id="author" class="field" required /><br /><br />',
+	'<button class="btnForm" id="search">Rechercher</button><br /><br />',
+	'<button class="btnForm" id="cancel">Annuler</button><br /><br />',
+	'</form>');
+	//'<input type="submit" id="submit" class="btnForm" value="Rechercher" /><br /><br />',
+	//'<input type="reset" id="reset" class="btnForm" value="Annuler" /><br /><br />',
 }*/
+
+});
