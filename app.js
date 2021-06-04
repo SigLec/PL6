05351db38ec3ddcb462008b0a49e249ef04ba73c
sig Lec <slecaudey@gmail.com>
2021-06-04 10:57:02 +0200

    add search result datas

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ var $searchForm = $('#searchForm');
 $searchForm.append(
 	'<form id="form">',
 	'<label for="title" class="label">Titre du livre</label><br />', 
-	'<input type="text" id="title" class="field" required /><br /><br />',
+	'<input type="text" id="titleInput" class="field" /><br /><br />',
 	'<label for="author" class="label">Auteur</label><br />', 
-	'<input type="text" id="author" class="field" required /><br /><br />',
+	'<input type="text" id="authorInput" class="field" /><br /><br />',
 	'<button class="btnForm" id="search">Rechercher</button><br /><br />',
 	'<button class="btnForm" id="cancel">Annuler</button><br /><br />',
 	'</form>');
@@ -34,34 +34,95 @@ $('#addBook').click (function(){
 $('#cancel').click (function(){
 	$searchForm.hide();
 	$addBook.show();
-	$searchResultsBlock.hide();
+	$resultsTitle.hide();
+	$resultsContainer.hide();
 	$field.val("");
 });
 
-// Ajout du bloc des résultats de la recherche  
-var addSearchResultsBlock = $('#searchForm').after('<div id="searchResultsBlock">Résultats de la recherche :</div>');
-var $searchResultsBlock = $('#searchResultsBlock');
+/* A partir d'ici, code appel API requête AJAX */
+$searchForm.after(
+	"<div class='h4' id='resultsTitle'>" +
+	"<h4>Résultats de recherche</h4>" +
+	"</div>");
+var $resultsTitle = $('#resultsTitle');
 
-// Ajout du bloc témoin pour chaque item de résultats
-$searchResultsBlock.append('<div id="resultBlock"></div>');
-var $resultBlock = $('#resultBlock');
+$resultsTitle.after(
+	"<div class='container' id='resultsContainer'></div>");
+var $resultsContainer = $('#resultsContainer');
+// Bloc div titre et résultats de la recherche caché par défaut
+$resultsTitle.hide();
+$resultsContainer.hide();
 
-// Ajout des renseignements à l'intérieur du bloc témoin
-$resultBlock.append(
-	'<p id="bookTitle" class="insideResultBlock">Titre : </p>',
-	'<p id="bookId" class="insideResultBlock">Id : </p>',
-	'<p id="bookAuthor" class="insideResultBlock">Auteur :</p>',
-	'<p id="bookDescription" class="insideResultBlock">Description :</p>',
-	'<img id="imgBook" class="img" src ="image/unavailable.png"/>'
-);
+//-----var $resultBlock = $('#resultBlock');-------------------------//
+var apiUrl = "https://www.googleapis.com/books/v1/volumes?q="
+var apiKey = "AIzaSyAdMSAMUsKSRpbMDjFuBmTjPDGTk4DRN-k"
+var resultsData;
 
-// Bloc div résultats de la recherche caché par défaut
-$searchResultsBlock.hide();
+// Ecoute du bouton search
+$('#search').click(function() {
+	//$resultsContainer.html("");
+	resultsData = $('#titleInput').val();
+	//handling empty search input field
+	if(resultsData === "" || resultsData === null) {
+		alert("Vous devez renseigner les champs titre et auteur avant de soumettre le formulaire");
+	} else {
+		$.ajax({
+			url: apiUrl + resultsData,
+			dataType: 'json',
+			success: function(response) {
+				console.log(response);
+				if(response.totalItems === 0) {
+					alert("Aucun livre n\'a été trouvé");
+				}
+				else {
+					$resultsTitle.show();
+					$resultsContainer.show();
 
-// Fonctionnalités du clic sur le bouton "Rechercher"
-$('#search').click (function(){
-	$searchResultsBlock.show();
+					displayResults(response);
+				}
+			},
+			error: function() {
+				alert("Une erreur est survenue");
+			}
+		});
+	}
+	$('#titleInput').val(""); // clean search box
 });
+
+	//Fonction to display the results in index.html
+	function displayResults(response) {
+		for (var i = 0; i < response.items.length; i++) {
+			// get id, title, author, description and image of the book
+			item = response.items[i];
+			var $title = item.volumeInfo.title;
+			var $id = item.id;
+			var $author = item.volumeInfo.authors[0];
+			/* The description should contain 200 char max and display 
+			"Information manquante" if there's no description available*/
+			var $description = 
+			item.volumeInfo.description === undefined ? 
+			"Information manquante" : item.volumeInfo.description.substr(0, 199);
+			var $imgBook = item.volumeInfo.imageLinks === undefined ?
+			"image/unavailable.png" : item.volumeInfo.imageLinks.thumbnail;
+
+	$('.container').append(
+			"<div class='container-card'>" +
+  			"<div class='card'>" +
+  			"<div class='card-body'>" +
+    			"<h5 class='card-title'>Titre :&nbsp;" + $title + "</h3>" +
+    			"<h4 class='card-author'>"  + $author  + "</h4>" +
+       		"<p class='card-id'>Id :&nbsp;" + $id  + "</p>" +
+       		"<p class='card-text'>Description : &nbsp;" + $description + "</p>" + 
+   			"<button class='button' id='bookmark'" + $id + "'><i class='far-solid fa-bookmark'></i></button>" +
+   			"<img src='" + $imgBook + "'class='card-img-bottom img-fluid img-thumbnail' alt='later'/>" + 
+			"</div>" +
+			"</div>" +
+			"</div>");
+
+
+			}
+	}
+
 
 
 });
