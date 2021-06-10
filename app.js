a775b19f40e1fde4e10b8ddf02c6fae40b3b7d72
sig Lec <slecaudey@gmail.com>
2021-06-10 11:19:09 +0200

    add functions for session storage + onclick bookmark event

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 $(document).ready(function(){
 
-// Ajout bouton "Ajouter un livre" 
+var book, title, author, id, description, imgBook;
+var booksSortedById = new Set();
+var selectedBooks = new Set();
+var pochList = [];
+
+
+// Add button "Ajouter un livre" 
 var addButton = $('.h2').after('<button class="btn" id="addBook">Ajouter un livre</button>');
 var $addBook = $('#addBook');
 	
-// Ajout bloc div "searchForm" 
+// Add div "searchForm" 
 var addSearchForm = $('.h2').after('<div id="searchForm"></div>');
 var $searchForm = $('#searchForm');
 
-// Ajout du formualire dans le bloc div "searchForm"
+// Add form in div "searchForm"
 $searchForm.append(
 	'<form id="form">',
 	'<label for="title" class="label">Titre du livre</label><br />', 
@@ -21,16 +27,16 @@ $searchForm.append(
 
 var $field = $('.field');
 
-// Bloc div formulaire caché par défaut
+// div form hidden by default
 $searchForm.hide();
 
-// Fonctionnalités du clic sur le bouton "Ajouter un livre"
+// Event listener on "addBook" button
 $('#addBook').click (function(){
 	$addBook.hide();
 	$searchForm.show();
 });
 
-// Fonctionnalités du clic sur le bouton "Annuler"
+// Event listener on "cancel" button
 $('#cancel').click (function(){
 	$searchForm.hide();
 	$addBook.show();
@@ -39,26 +45,30 @@ $('#cancel').click (function(){
 	$field.val("");
 });
 
-/* A partir d'ici, code appel API requête AJAX */
+// Add div for title of the search results area
 $searchForm.after(
 	"<div class='h4' id='resultsTitle'>" +
 	"<h4>Résultats de recherche</h4>" +
 	"</div>");
 var $resultsTitle = $('#resultsTitle');
-
+// Add div for search results area
 $resultsTitle.after(
 	"<div class='container' id='resultsContainer'></div>");
 var $resultsContainer = $('#resultsContainer');
-// Bloc div titre et résultats de la recherche caché par défaut
+// Title and search results block hidden dy default
 $resultsTitle.hide();
 $resultsContainer.hide();
 
 var apiUrl = "https://www.googleapis.com/books/v1/volumes?q="
 var apiKey = "AIzaSyAdMSAMUsKSRpbMDjFuBmTjPDGTk4DRN-k"
-var resultsData;
 
-// Ecoute du bouton search
+// Event listener on search button
 $('#search').click(function() {
+	apiCall();
+});
+
+//Send the request to google API
+	function apiCall() {
 	$resultsContainer.html("");
 	titleInput = $('#titleInput').val();
 	authorInput = $('#authorInput').val();
@@ -67,18 +77,17 @@ $('#search').click(function() {
 		alert("Vous devez renseigner les champs titre et auteur avant de soumettre le formulaire");
 	} else {
 		$.ajax({
-			url: apiUrl + titleInput + "+inauthor:" +authorInput,
+			url: apiUrl + "+intitle:" + titleInput + "+inauthor:" + authorInput,
 			dataType: 'json',
 			success: function(response) {
 				console.log(response);
 				if(response.totalItems === 0) {
-					alert("Aucun livre n\'a été trouvé");
+					alert("Aucun livre n'a été trouvé");
 				}
 				else {
 					$resultsTitle.show();
 					$resultsContainer.show();
-
-					displayResults(response);
+					displayResponse(response);
 				}
 			},
 			error: function() {
@@ -88,42 +97,65 @@ $('#search').click(function() {
 	}
 	$('#titleInput').val(""); // clean search input
 	$('#authorInput').val("");  // clean search input
-});
+}
 
-	//Fonction to display the results in index.html
-	function displayResults(response) {
-		for (var i = 0; i < response.items.length; i++) {
-			// get id, title, author, description and image of the book
-			item = response.items[i];
-			var $title = item.volumeInfo.title;
-			var $id = item.id;
-			var $author = item.volumeInfo.authors[0];
-			/* The description should contain 200 char max and display 
-			"Information manquante" if there's no description available*/
-			var $description = 
-			item.volumeInfo.description === undefined ? 
-			"Information manquante" : item.volumeInfo.description.substr(0, 199);
-			var $imgBook = item.volumeInfo.imageLinks === undefined ?
-			"image/unavailable.png" : item.volumeInfo.imageLinks.thumbnail;
-
-	$resultsContainer.append(
-			"<div class='container-card'>" +
-  			"<div class='card'>" +
-  			"<div class='card-body'>" +
-    			"<h5 class='card-title'>Titre :&nbsp;" + $title + "</h3>" +
-    			"<h4 class='card-author'>"  + $author  + "</h4>" +
-       		"<p class='card-id'>Id :&nbsp;" + $id  + "</p>" +
-       		"<p class='card-text'>Description : &nbsp;" + $description + "</p>" + 
-   			"<button class='button' id='bookmark'" + $id + "'><i class='far-solid fa-bookmark'></i></button>" +
-   			"<img src='" + $imgBook + "'class='card-img-bottom img-fluid img-thumbnail' alt='later'/>" + 
-			"</div>" +
-			"</div>" +
-			"</div>");
-
-
-			}
+//Fonction to display the results in index.html
+function displayResponse(response) {
+	var result = [];
+	for (var i = 0; i < response.items.length; i++) {
+		var item = response.items[i];
+		var book = {
+			"title": item.volumeInfo.title,
+			"author" : item.volumeInfo.authors[0],
+			"id": item.id,
+			"description": item.volumeInfo.description ? item.volumeInfo.description.substr(0, 199) : "information manquante",
+			"imgBook" : item.volumeInfo.imageLinks ? item.volumeInfo.imageLinks.thumbnail :
+		"image/unavailable.png" 
+		};
+		result.push(book);
+		booksSortedById[book.id] = book;
+		}
+		displayResponseInCard(result);
 	}
 
+//Fonction to display the results in cards format
+function displayResponseInCard(result) {
+	for(var i = 0; i < result.length; i++) {
+    var book = result[i];
+	$resultsContainer.append(
+		'<div class="container-card">'+
+  		'<div class="card">' +
+  		'<div class="card-body">' +
+    		'<h5 class="card-title" id="titleCard">Titre :&nbsp;' + book.title + '</h3>' +
+    		'<h4 class="card-author" id="authorCard">'  + book.author + '</h4>' +
+       	'<p class="card-id">Id :&nbsp;' + book.id  + '</p>' +
+       	'<p class="card-text">Description : &nbsp;' + book.description + "&nbsp;[...]" + '</p>' + 
+				"<button id='bookmark' data-id= '" + book.id + "'><i class='fas fa-bookmark'></i></button>" +   		
+				'<img src="' + book.imgBook + '"class="card-img-bottom img-fluid img-thumbnail" alt="cover"/>' + 
+		'</div>' +
+		'</div>' +
+		'</div>');
+	}
+}
 
+	$("body").on("click", "#bookmark", function() {
+				bookId = $(this).data("id");
+				console.log(bookId);
+				addToFav(bookId);
+	});
+
+			function addToFav(bookId) {
+			var book = booksSortedById[bookId];
+			if(selectedBooks.has(bookId)) {
+			console.log('Vous ne pouvez ajouter deux fois le même livre');
+			} else {
+			selectedBooks.add(bookId);
+			pochList.push(book);
+			console.log('Votre livre a bien été ajouté à votre poch\'List!');
+			sessionStorage.setItem('selectedBooks', JSON.stringify(pochList));
+			console.log(selectedBooks);
+			console.log(pochList);
+		}
+	}
 
 });
